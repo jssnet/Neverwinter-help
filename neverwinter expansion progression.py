a2687d712cbbd01a250f7b998069913d157774fc
the-construction-worker <22092182+the-construction-worker@users.noreply.github.com>
2018-03-27 23:07:57 -0400

    Initial commit

---
+++ neverwinter expansion progression.py
@@ -0,0 +1,59 @@
+'''
+Created on 2017-01-30 January 30th, 2017
+worked on 2017-01-30 07-17
+'''
+#stats
+'''
+the stats with two example Deflect Deflect gets ****** *** + *** IT, Adds!!! Rogue DEX + CHA.
+#boons
+'''
+'''
+(example)
+- choosing boons, can you choose both:  Life Steal, Deflect.
+- _HP_, _Deflect_, ???
+'''
+'''
+Equipment vs At-Wills "equipment stat advantage"
+"base stats" HP
+"extra helpers" _Crit Severity_ _Deflect Severity_ _Life Steal Severity_
+'''
+#Rogue
+'''
+non-range "Bard" would be, "_no Shady" <-- non-Daze
+'''
+'''
+- control Hunters
+bake *20 12 *24 12 /18 12
+happy /16 *16 /23 12 *21 12
+bleud
+
+bake:  "_Stamina+_" "_Deflect+_", _Control_ with *CRIT* <-- has _resistance ignored_.
+happy: Stamina, HP+, "_Deflect+_" "Control+" "Crit+" } also, base At-Will damage.
+
+- no control Rogue
+- time:  Encounter
+bard /17 /11 *24 13 -- *21
+time *21 13 20 *15 -- 19
+main *17 -- *19 -- -- 14
+
+bard:  "_Deflect++++_" "Stamina" "~HP" "_Crit+_" "__Combat Advantage++__" (with '_Tenacious_')
+time:  "_Stamina+_" "_BASE STR DAM_" "low _crit_, low _Deflect_" ("_supplemental base damage_")
+
+- pewpew is control
+- pewpew:  max artifact weapon
+- pewpew:  Encounter, _AP_.
+- mewtoo, tehteh:  At-Wills } "Valhalla"
+
+3x Warlocks:  _crit_ + _Combat Advantage_
+pewpew:  Stamina, Deflection, Control
+mewtoo:  _Crit_ _At-Wills_
+tehteh:  _HP_, _basedamage_ with _At-Wills_
+
+pewpew 12 /18 12 *23 12 *23
+mewtoo -- *14 -- /18 -- *20
+tehteh -- *18 -- /12 -- *18
+'''
+
+if __name__ == '__main__' or '__runpy__':
+	print("Neverwinter Statistics Expansion Progression")
+	
---
+++ neverwinter glowy power feats selector.py
@@ -0,0 +1,13 @@
+'''
+Created on 2017-07-14 July 14th, 2017
+worked on 2017-07-14
+'''
+'''
+item level gained/ queue
+--> based on existing _gear_
+- prefer draconics, vicious etc.
+"The Maze Engine" is _first_.
+- plan boons like _going_ example #3 Shandanar is 3% _Action Points_.
+
+/written "core ability gain"
+'''
